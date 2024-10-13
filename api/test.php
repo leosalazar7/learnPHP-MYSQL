5a528d19acde0a7e8b1672f1d3c6923e6762e1ac
Leo Salazar <leosalazar137@gmail.com>
2024-10-13 16:39:36 -0400

    idk

---
+++ api/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Document</title>
+    </head>
+    <body>
+        <h1><?php echo '<b>Hello</b>' ?></h1>
+
+
+        <script src="scripts.js"></script>
+    </body>
+</html>
